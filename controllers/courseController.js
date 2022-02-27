31003fa01d3127e58567545d424718e3611366b4
missbolin <samanthabolin1@gmail.com>
2022-02-26 16:43:43 -0800

    base app

---
+++ controllers/courseController.js
@@ -0,0 +1,105 @@
+const {Course, Student, StudentCourses} = require('../models')
+const departments = ['Math', 'English', 'Music', 'Art', 'PE', 'World Languages', 'Social Studies', 'Science'].sort();
+
+//view all
+module.exports.viewAll = async function(req,res){
+    const courses = await Course.findAll();
+    res.render('course/view_all', {courses});
+}
+
+//profile
+module.exports.viewProfile= async function(req,res){
+    const course = await Course.findByPk(req.params.id, {
+        include: 'students'
+    });
+    const students = await Student.findAll();
+    let availableStudents = [];
+    for (let i=0; i<students.length; i++){
+        if (!courseHasStudent(course, students[i])){
+            availableStudents.push(students[i])
+        }
+    }
+    res.render('course/profile', {course, availableStudents})
+}
+
+//render add form
+module.exports.renderAddForm =  function(req, res){
+    const course =  {
+        name: '',
+        department: departments[0],
+        instructor_name: '',
+        description:''
+    }
+    res.render('course/add', {course, departments});
+}
+
+//add
+module.exports.addCourse = async function(req, res){
+    const course = await Course.create({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+    });
+    res.redirect(`/courses/profile/${course.id}`);
+}
+
+//render edit form
+module.exports.renderEditForm = async function(req, res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/edit', {course, departments});
+}
+
+//update
+module.exports.updateCourse = async function(req, res){
+    const course = await Course.update({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+        }, {
+        where: {
+            id: req.params.id
+        }
+        });
+    res.redirect(`/courses/profile/${req.params.id}`);
+}
+
+//delete
+module.exports.deleteCourse = async function(req, res){
+    await Course.destroy({
+        where: {
+            id:req.params.id
+        }
+        });
+    res.redirect('/courses');
+}
+
+//Add student to a course
+module.exports.enrollStudent = async function(req, res){
+    await StudentCourses.create({
+        student_id: req.body.student,
+        course_id: req.params.courseId
+    });
+    res.redirect(`/courses/profile/${req.params.courseId}`)
+}
+
+//remove a student from a course
+module.exports.removeStudent = async function(req, res){
+    await StudentCourses.destroy({
+        where: {
+            course_id: req.params.courseId,
+            student_id: req.params.studentId
+        }
+    });
+    res.redirect(`/courses/profile/${req.params.courseId}`);
+}
+
+function courseHasStudent(course, student) {
+    for (let i = 0; i < course.students.length; i++) {
+        if (student.id === course.students[i].id) {
+            return true
+        }
+    }
+    return false
+}
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentController.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
+router.get('/courses/add', courseController.renderAddForm);
+router.post('/courses/add', courseController.addCourse);
+router.get('/courses/delete/:id', courseController.deleteCourse);
+
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.post('/students/edit/:id', studentController.updateStudent);
+router.get('/students/add', studentController.renderAddForm);
+router.post('/students/add', studentController.addStudent);
+router.get('/students/delete/:id', studentController.deleteStudent);
+
+router.post('/students/:studentId/enroll/', studentController.enrollStudent);
+router.get('/students/:studentId/removeCourse/:courseId', studentController.removeCourse);
+router.post('/courses/:courseId/enroll', courseController.enrollStudent);
+router.get('/courses/:courseId/removeStudent/:studentId', courseController.removeStudent);
+
+
+module.exports = router;
